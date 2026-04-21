10234c3a7d68411a6dc8bd52818e7be4ebc76df1
Coder-Heinz-Delta <20240263@ctucareer.co.za>
2026-04-21 09:36:13 +0200

    Add FarmerLocation struct and update project files
    - Introduced a new struct `FarmerLocation` to represent farm details including name, province, and town.
    - Updated project file list to include new sourcelink.json and FarmerLocation.cs.
    - Modified various binary files and cache files as part of the build process.

--- obj/Debug/net10.0/Farmers_Market_API.AssemblyInfo.cs
+++ obj/Debug/net10.0/Farmers_Market_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmers_Market_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5dfc908f0d3c2d09a853cfd0a0370fc3c977279")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmers_Market_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmers_Market_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
