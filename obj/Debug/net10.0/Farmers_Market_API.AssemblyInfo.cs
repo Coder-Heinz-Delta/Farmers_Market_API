966d2be3b0025fbc983eebf314a08d9709c2f057
Coder-Heinz-Delta <20240263@ctucareer.co.za>
2026-04-28 09:17:17 +0200

    skibidi

--- obj/Debug/net10.0/Farmers_Market_API.AssemblyInfo.cs
+++ obj/Debug/net10.0/Farmers_Market_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmers_Market_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10234c3a7d68411a6dc8bd52818e7be4ebc76df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c7d8b9c01a176c213d22bce6341d464915f725")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmers_Market_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmers_Market_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
